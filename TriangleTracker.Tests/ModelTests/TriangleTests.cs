fa2e53c7b11b81bd9f7eb3df2bc65a2838df9dfd
Alex Adamovic <alexanderadamovic@gmail.com>
2022-02-22 10:54:15 -0800

    add test for isoceles for TriangleType with good fail
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>

--- TriangleTracker.Tests/ModelTests/TriangleTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -39,13 +39,23 @@ namespace TriangleTracker.Tests
     }
 
     [TestMethod]
-    public void TriangleType_TriangleIsIsoceles_Equilateral()
+    public void TriangleType_TriangleIsEquilateral_Equilateral()
     {
       int sideA = 4;
       int sideB = 4;
       int sideC = 4;
       Triangle testTriangle = new Triangle(sideA, sideB, sideC);
       Assert.AreEqual("equilateral", testTriangle.TriangleType());
+    }
+
+        [TestMethod]
+    public void TriangleType_TriangleIsIsoceles_Isoceles()
+    {
+      int sideA = 4;
+      int sideB = 4;
+      int sideC = 2;
+      Triangle testTriangle = new Triangle(sideA, sideB, sideC);
+      Assert.AreEqual("isoceles", testTriangle.TriangleType());
     }
   }
 }
